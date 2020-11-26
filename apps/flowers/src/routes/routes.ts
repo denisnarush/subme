39c9c0253643f0ff87011ca58ab707f49b9a7bd4
Denis Narush <child.denis@gmail.com>
2020-11-26 21:06:54 +0300

    updates routes

--- apps/flowers/src/routes/routes.ts
+++ apps/flowers/src/routes/routes.ts
@@ -2,16 +2,16 @@ import { Routes } from '@angular/router';
 
 export const SRoutes: Routes = [
   {
-    path: '',
-    redirectTo: '/home',
-    pathMatch: 'full',
-  },
-  {
     path: 'home',
     loadChildren: () =>
       import('@s/flowers/pages/feature-home').then(
         (m) => m.FlowersPagesFeatureHomeModule
       ),
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
   },
   {
     path: '**',
