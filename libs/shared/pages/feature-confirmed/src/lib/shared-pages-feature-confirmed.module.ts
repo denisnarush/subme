eff1b300ee2e07b40000048b8d5e964bbb48db15
DeLaut <54510364+DeLaut@users.noreply.github.com>
2020-12-01 12:09:30 +0300

    #17 updates Payment confirmed page template and fixes routing with Confirmed page

--- libs/shared/pages/feature-confirmed/src/lib/shared-pages-feature-confirmed.module.ts
+++ libs/shared/pages/feature-confirmed/src/lib/shared-pages-feature-confirmed.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
 import { FeatureConfirmedComponent } from './feature-confirmed/feature-confirmed.component';
 import { UiContentModule } from '@s/ui/ui-content';
 import { UiFooterModule } from '@s/ui/ui-footer';
@@ -10,7 +9,6 @@ import { UiFooterModule } from '@s/ui/ui-footer';
     CommonModule,
     UiContentModule,
     UiFooterModule,
-    RouterModule.forChild([{ path: '', component: FeatureConfirmedComponent }]),
   ],
   declarations: [FeatureConfirmedComponent],
   exports: [FeatureConfirmedComponent],
