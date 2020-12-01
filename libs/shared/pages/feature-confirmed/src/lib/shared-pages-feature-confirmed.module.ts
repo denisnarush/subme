163b2e08257a085e9c643667926dea85ffe635eb
DeLaut <54510364+DeLaut@users.noreply.github.com>
2020-12-01 21:28:29 +0300

    #29 updates styles and template to pixel perfect and adds scenario for payment-confirmed page backstop testing

--- libs/shared/pages/feature-confirmed/src/lib/shared-pages-feature-confirmed.module.ts
+++ libs/shared/pages/feature-confirmed/src/lib/shared-pages-feature-confirmed.module.ts
@@ -3,13 +3,10 @@ import { CommonModule } from '@angular/common';
 import { FeatureConfirmedComponent } from './feature-confirmed/feature-confirmed.component';
 import { UiContentModule } from '@s/ui/ui-content';
 import { UiFooterModule } from '@s/ui/ui-footer';
+import { UiHeaderModule } from '@s/ui/ui-header';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    UiContentModule,
-    UiFooterModule,
-  ],
+  imports: [CommonModule, UiContentModule, UiFooterModule, UiHeaderModule],
   declarations: [FeatureConfirmedComponent],
   exports: [FeatureConfirmedComponent],
 })
