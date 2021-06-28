fa55091e731dbfcdcb9bfe20e997ec1fabe402a0
Denis Narush <child.denis@gmail.com>
2021-06-29 00:06:22 +0300

    #75 updates according to lint

--- libs/ui/ui-tab/src/lib/ui-tab/ui-tab.component.ts
+++ libs/ui/ui-tab/src/lib/ui-tab/ui-tab.component.ts
@@ -14,13 +14,13 @@ import {
   encapsulation: ViewEncapsulation.None,
 })
 export class UiTabComponent {
-  @Input() isSelected: boolean = false;
-  @Output() onSelected = new EventEmitter<number>();
+  @Input() isSelected = false;
+  @Output() activate = new EventEmitter<number>();
 
   constructor(private elRef: ElementRef) {}
 
-  select(): void {
-    this.onSelected.emit(
+  onClick(): void {
+    this.activate.emit(
       [].slice
         .call(this.elRef.nativeElement.parentElement.children)
         .indexOf(this.elRef.nativeElement)
