6f4abba8fe6fd7c09e02689b67f09a2886995d88
Denis Narush <child.denis@gmail.com>
2022-06-27 17:45:41 +0300

    #36 Fixes slider for order page in landscape view
    * #36 fixes slider for order page in landscape view

--- libs/ui/ui-modal/src/lib/ui-modal/ui-modal.component.spec.ts
+++ libs/ui/ui-modal/src/lib/ui-modal/ui-modal.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import exp = require('constants');
 
 import { UiModalComponent } from './ui-modal.component';
 
@@ -8,9 +9,8 @@ describe('UiModalComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ UiModalComponent ]
-    })
-    .compileComponents();
+      declarations: [UiModalComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -21,5 +21,41 @@ describe('UiModalComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Has "isActive(): boolean" method which returns boolean value represented state', () => {
+    expect(component.isActive).toBeDefined();
+    expect(typeof component.isActive() === 'boolean').toBe(true);
+    expect(component.isActive()).toBe(false);
+  });
+
+  it('Has method "toggle(): void" which changes "active" state', () => {
+    expect(component.toggle).toBeDefined();
+    expect(component.toggle()).toBeUndefined();
+    expect(component.isActive()).toBe(true);
+    component.toggle();
+    expect(component.isActive()).toBe(false);
+  });
+
+  it('Has method "activate(): void" which sets state to be active', () => {
+    expect(component.activate).toBeDefined();
+    expect(component.activate()).toBeUndefined();
+    expect(component.isActive()).toBe(true);
+    component.activate();
+    expect(component.isActive()).toBe(true);
+    component.toggle();
+    component.activate();
+    expect(component.isActive()).toBe(true);
+  });
+
+  it('Has method "deactivate(): void" which sets state to be inactive', () => {
+    expect(component.deactivate).toBeDefined();
+    expect(component.deactivate()).toBeUndefined();
+    expect(component.isActive()).toBe(false);
+    component.deactivate();
+    expect(component.isActive()).toBe(false);
+    component.toggle();
+    component.deactivate();
+    expect(component.isActive()).toBe(false);
   });
 });
